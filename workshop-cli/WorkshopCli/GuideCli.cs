49c7ddbbcb4f0c8c68d0aaccc52c01ac55b5223e
JohnKouachi <fariajorge@ipvc.pt>
2023-04-14 09:25:51 +0100

    improve guide for cli, color add and format

--- workshop-cli/WorkshopCli/GuideCli.cs
+++ workshop-cli/WorkshopCli/GuideCli.cs
@@ -1,5 +1,6 @@
 ﻿using Sharprompt;
 
+
 namespace workshop_cli;
 
 public class GuideCli
@@ -13,36 +14,47 @@ public class GuideCli
 
     public void Run()
     {
-        Console.WriteLine("Welcome to the programming workshop!");
-        Console.WriteLine();
+        Console.WriteLine("Bem vindo ao Workshop de Love2D");
+        Console.WriteLine("Pra começar reponde a algumas perguntas");
+        
+       
 
         foreach (var step in guide.Steps)
         {
             Console.WriteLine(step.Message);
+            
 
             switch (step.Type)
             {
                 case "question":
                     var answer = Prompt.Input<string>(":");
-                    Console.WriteLine($"You entered: {answer}");
+                    Console.WriteLine($"Resposta: {answer}");
                     break;
                 
                 case "information":
-                    Prompt.Confirm("Did you complete the task?", false);
-                    Console.WriteLine("lets work!");
+                    Prompt.Confirm("Quando completares a taréfa Avança para a frente", false);
+                    Console.WriteLine("");
                     break;
 
-              
+
                 case "exercise":
                     while (true)
                     {
-                        Prompt.Confirm("Did you complete the exercise?", false);
-                        var input = Prompt.Input<string>("Type 'finish' to move to the next exercise", "");
-                        if (input.ToLower() == "finish")
-                        {
-                            Console.WriteLine("Great job!");
-                            break;
-                        }
+                        //Prompt.Confirm("Já acabaste o exercicio? ", false);
+                        var input = Prompt.Input<string>("Quando acabares O exercicio escreve 'acabei' na consola", "");
+                        if (input.ToLower() != "y") continue;
+                        Console.WriteLine("Great job!");
+                        break;
+                    }
+
+                    break;
+                case "challenge":
+                    while (true)
+                    {
+                        var inputCh = Prompt.Input<string>("Quando acabares o desafio escreve 'acabei' na consola", "");
+                        if (inputCh.ToLower() != "y") continue;
+                        Console.WriteLine("Great job!");
+                        break;
                     }
 
                     break;
--- workshop-cli/WorkshopCli/Program.cs
+++ workshop-cli/WorkshopCli/Program.cs
@@ -5,13 +5,13 @@ using workshop_cli;
 
 
 
-string jsonFilePath = "C:/Users/jrafa/Desktop/REPO/joworkshop-cli/workshop-cli/Steps.json";
-string jsonText = File.ReadAllText(jsonFilePath);
-Console.WriteLine(jsonText);
-        
-var  steps = JsonConvert.DeserializeObject<List<Guide.Step>>(jsonText);
-        
-Guide guide = new Guide { Steps = steps };
+    var jsonFilePath = "C:/Users/jrafa/Desktop/REPO/workshop-cli/workshop-cli/Steps.json";
+    var jsonText = File.ReadAllText(jsonFilePath);
+    //Console.WriteLine(jsonText);
+            
+    var  steps = JsonConvert.DeserializeObject<List<Guide.Step>>(jsonText);
+            
+    var guide = new Guide { Steps = steps };
 
-var guideCli = new GuideCli(guide);
-guideCli.Run();
+    var guideCli = new GuideCli(guide);
+    guideCli.Run();
