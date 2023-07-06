54a60f9f4c7cd4418036ea2e08ac6b70039fe0cb
Francisco Costa <frcosta@ipvc.com>
2023-07-06 17:38:45 +0100

    Commit teste

--- Resources/git_loop.sh
+++ Resources/git_loop.sh
@@ -5,7 +5,7 @@ script_dir=$(dirname "$(readlink -f "$0")")
 json_file="session.txt"
 name=$(grep -o '"Name":"[^"]*' "$json_file" | cut -d '"' -f 4)
 # Construct the SSH key path
-ssh_key_path="$script_dir/my_repo_deploy_key"
+ssh_key_path="$script_dir"
 
 # Start SSH agent and add SSH key
 eval "$(ssh-agent -s)"
@@ -14,7 +14,7 @@ ssh-add $ssh_key_path
 git checkout $name
 
 # Add files to commit
-git add .
+git add workshop2023/teste.txt
 
 # Create a commit
 git commit -m "Commit $name"
