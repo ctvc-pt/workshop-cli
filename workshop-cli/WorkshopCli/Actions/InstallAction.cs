870af8f138235d48b3e0b9b84e10a92c1a3c9d14
Francisco Costa <franciscojose1247@gmail.com>
2024-06-21 14:45:24 +0100

    Check if Love2D is installed

--- workshop-cli/WorkshopCli/Actions/InstallAction.cs
+++ workshop-cli/WorkshopCli/Actions/InstallAction.cs
@@ -24,16 +24,20 @@ public class InstallAction : IAction
         }
         
         var exePath = Path.Combine( "C:\\Program Files\\LOVE\\love.exe" );
-        var lovePath = Path.Combine( GuideCli.ResourcesPath,"love-11.4-win64.exe" );
-        Process loveProcess = new Process();
-        loveProcess.StartInfo.FileName = lovePath;
-        loveProcess.StartInfo.Arguments = $"\"{lovePath}\" /SILENT /NORESTART /SP-";
-        loveProcess.StartInfo.Verb = "runas";
-        loveProcess.Start();
-        loveProcess.WaitForExit();
         if ( !File.Exists(exePath) )
         {
+            var lovePath = Path.Combine( GuideCli.ResourcesPath,"love-11.4-win64.exe" );
+            Process loveProcess = new Process();
+            loveProcess.StartInfo.FileName = lovePath;
+            loveProcess.StartInfo.Arguments = $"\"{lovePath}\" /SILENT /NORESTART /SP-";
+            loveProcess.StartInfo.Verb = "runas";
+            loveProcess.Start();
+            loveProcess.WaitForExit();
             Execute();
+        }
+        else
+        {
+            Console.WriteLine("O Love2D está instalado.");
         }
     }
 }
