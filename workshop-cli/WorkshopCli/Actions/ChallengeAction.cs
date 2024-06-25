2936e59ee21f09719eebce561dc948f5af9ab663
Francisco Costa <franciscojose1247@gmail.com>
2024-06-25 15:19:55 +0100

    Update commit system to save files after every challenge

--- workshop-cli/WorkshopCli/Actions/ChallengeAction.cs
+++ workshop-cli/WorkshopCli/Actions/ChallengeAction.cs
@@ -5,15 +5,20 @@ namespace workshopCli;
 public class ChallengeAction : IAction
 {
     public int Delay;
+    public string SessionName;
 
-    public ChallengeAction( int delay )
+    public ChallengeAction( int delay, string sessionName )
     {
         Delay = delay;
+        SessionName = sessionName;
     }
     public void Execute()
     {
         Thread.Sleep(Delay);
         Console.ForegroundColor = ConsoleColor.Yellow;
         ExerciseHelper.PromptAnswerAndConfirm("Quando completares o desafio, escreve 'proximo' ou 'p' para avançar ou para retroceder escreve 'anterior' (ou 'ajuda')\n");
+        
+        var repoManager = new GitHubManager();
+        repoManager.Commit(SessionName);
     }
 }
