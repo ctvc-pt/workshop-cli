a7f3abd9b856cd5f9c1868c9bdc0fedb5645c155
Luis Afonso <luis.manuel.afonso@ipvc.pt>
2025-03-07 17:53:41 +0000

    Add check if ollama and vs code are installed instead of install

--- Resources/backup.lua
+++ Resources/backup.lua
@@ -1,4 +1,16 @@
-function love.draw()
-        love.graphics.print("Ola mundo!", 100, 100);
+function love.load()
+    love.window.setMode(1200, 960) -- Define o tamanho da janela do jogo
+    --Escreve o código para carregar a imagem na linha baixo
+
 end
 
+
+function love.draw()
+    love.graphics.print("Ola mundo!", 200, 200);
+    --Escreve o código para desenhar a imagem na linha baixo
+
+end
+
+function love.update(dt)
+
+end
