5aebda292a6eccbc5ccd72a88ff72d0289216126
Luis Afonso <luis.manuel.afonso@ipvc.pt>
2025-03-10 17:54:57 +0000

    Fix Double Enter

--- Resources/backup.lua
+++ Resources/backup.lua
@@ -1,16 +1,4 @@
-function love.load()
-    love.window.setMode(1200, 960) -- Define o tamanho da janela do jogo
-    --Escreve o código para carregar a imagem na linha baixo
-
-end
-
-
 function love.draw()
-    love.graphics.print("Ola mundo!", 200, 200);
-    --Escreve o código para desenhar a imagem na linha baixo
-
+        love.graphics.print("Ola mundo!", 100, 100);
 end
 
-function love.update(dt)
-
-end
